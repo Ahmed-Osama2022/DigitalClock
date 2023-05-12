fcc7cfb63a0b321ef8c316bf503502155f0a8715
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2023-05-13 02:01:11 +0300

    Edit the sw file

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,54 @@ workbox.routing.registerRoute(
     new workbox.strategies.CacheFirst()
     // new workbox.strategies.NetworkFirst()
 );
+
+
+
+// ============================= CACHING FILES ===================== //
+var cacheName = 'app-shell-cache-v1';
+var filesToCache = [
+    '/',
+    '/index.html',
+    './images/logo.png',
+    './images/watch.jpg',
+    './main.js',
+    './manifest.json'     
+]; 
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+     caches.open(cacheName)
+     .then(function (cache) {
+       return cache.addAll(filesToCache);
+       })
+       .then(function () {
+       return self.skipWaiting();
+       })
+      );
+    });
+
+
+self.addEventLister('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(keyList.map(key => { 
+                if (key !== cacheName) {
+                    return cache.delete(key);
+                    }
+                }));
+            }));
+            return self.clients.claim();
+            });
+
+
+
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+        return response || fetch(event.request);
+        })
+        );
+    });
+
+
--- test.js
+++
@@ -1,5 +0,0 @@
-workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
-    // new workbox.strategies.NetworkFirst()
-);
